a5e3a10b23445799a3145870a8507122db837194
gabriel santos <163359098+gabssiin@users.noreply.github.com>
2024-09-27 03:03:24 +0000

    a

--- apps.js
+++ apps.js
@@ -1,8 +1,9 @@
-
 var a = document.getElementById('area')
 a.addEventListener('click',clicar)
 a.addEventListener('mouseenter',entrar)
 a.addEventListener('mouseout',sair)
+
+
 function clicar(){ var a = document.getElementById('area')
 a.innerText='Clicou!'
 a.style.background= 'red'
@@ -13,4 +14,14 @@ a.innerText='entrou'
 }
 function sair(){var a = document.getElementById('area')
     a.innerText='saiu'
+}
+9
+
+function soma(){
+    var t1 = document.getElementById('txtn1')
+   var t2 = document.querySelector('input#txtn2')
+   var n1= Number(t1.value)
+   var n2= Number(t2.value)
+   var s = n1 + n2
+  
 }
